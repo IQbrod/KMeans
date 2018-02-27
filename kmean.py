c40d99c858a13f5c2d2b63ea5ea85889933ae62d
IQbrod <iqbrod@outlook.fr>
2018-02-27 10:33:00 +0100

    Adaptation for subject given by teachers

--- kmean.py
+++ kmean.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import math
+from sklearn.datasets import load_iris
 
 class ArgError(Exception):
 	pass
@@ -15,12 +16,11 @@ gEXT = ".txt"
 gREP = "files/"
 
 #Checking Arguments
-if (len(sys.argv) != 3):
-	raise ArgError("usage : kmean.py [K] [FileName]")
+if (len(sys.argv) != 2):
+	raise ArgError("usage : kmean.py [K]")
 
 #Getting Arguments
 gK = sys.argv[1]
-gFName = sys.argv[2]
 
 #Checking arguments conformity
 try:
@@ -28,41 +28,18 @@ try:
 except:
 	raise ArgError("[K] must be an integer")
 
-if not ((gFName.endswith(gEXT) and ("." not in gFName[:-len(gEXT)])) or ("." not in gFName)):
-	raise ArgError("[FileName] has unsupported extension => use "+gEXT)
-if not (gFName.startswith(gREP) or ("/" not in gFName)):
-	raise ArgError("[FileName] has unsupported path => use "+gREP)
-
-# Conforming gFName
-if ("." not in gFName):
-	gFName = gFName + gEXT
-if not (gFName.startswith(gREP)):
-	gFName = gREP + gFName
-
 ## MAIN ##
-repMatrix = []
-with open(gFName, "r") as gFile:
-	lFirstLine = gFile.readline().split(",")
-	lPageSize = int(lFirstLine[0])
-	lVocSize = int(lFirstLine[1])
+repMatrix = load_iris().data
 
-	if lPageSize < gK:
-		raise ArgError("[K] "+str(gK)+" is too high for "+sys.argv[2]+" => "+str(lPageSize)+" pages")
+# Calculing matrix size
+lPageSize = len(repMatrix)
+lVocSize = len(repMatrix[0])
 
-	# For each page	
-	for j in range(0,lPageSize):
-		ligne = gFile.readline().split(" ")	
-		ligneflt = []
-		# For each word
-		for i in range(1,lVocSize+1):
-			ligneflt.append(0) #0 in any case
-			for el in range(1,len(ligne)):
-				if (ligne[el].startswith(str(i)+":")): #If words[i] is in page
-					ligneflt[i-1] = float(ligne[el][2:]) #We replace 0 by prob
-					break
-		repMatrix.append(ligneflt)
+# Checking K is lower than documents size
+if lPageSize < gK:
+	raise ArgError("[K] "+str(gK)+" is too high => "+str(lPageSize)+" pages")
 
-# -- Display File as Matrix
+# -- Display Matrix
 print "Representation Matrix:"
 for i in range(0,len(repMatrix)):
 	print repMatrix[i]
